8cc56ee8bdd3f4b4dc8c1a8323970e17cc7ab195
Alishba Naseem <syednaseem995@gmail.com>
2024-04-05 21:39:42 +0500

    100-days-challenge

--- 11 to 20 days/20day.ts
+++ 11 to 20 days/20day.ts
@@ -30,15 +30,15 @@
 
 //Question 60: Self-Running User Profile: Create a quick, self-setup profile for a user that can tell you the user's name and age.
 
-let userProfile =(function() {
-     let name = "Aiman";
-     let age = 20;
+// let userProfile =(function() {
+//      let name = "Aiman";
+//      let age = 20;
 
-   return{
-    displayinfo: function(){
-        console.log(`Name: ${name} , Age:${age}`);
-    }
-   };
+//    return{
+//     displayinfo: function(){
+//         console.log(`Name: ${name} , Age:${age}`);
+//     }
+//    };
 
-})();
-userProfile.displayinfo();
+// })();
+// userProfile.displayinfo();
